636a8d14a065807b9b1a5453fc91c4e512323290
Galen Shirey <galenshirey@gmail.com>
2020-03-01 21:48:31 -0500

    Re-commit

---
+++ SafelySent/SafelySent/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SafelySent
+//
+//  Created by Galen Shirey on 3/1/20.
+//  Copyright © 2020 Galen Shirey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("Hello")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
