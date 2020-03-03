fad35e0877905f5339a06f71b609e82ec80207ec
gjshirey <galenshirey@gmail.com>
2020-03-03 00:04:14 -0500

    added authentication

--- app.js
+++ app.js
@@ -4,17 +4,40 @@ const txtEmail = document.getElementById('txtEmail')
 const txtPassword = document.getElementById('txtPassword')
 const btnLogin = document.getElementById('btnLogin')
 const btnSignUp = document.getElementById('btnSignUp')
-const btnLogout = document.getElementById('btnLogout')
+const logout = document.getElementById('logout')
 
-
-//add login event
+////////////////////////////////////
+// button Login - EVENT LISTENER
+// add login event
+////////////////////////////////////
 btnLogin.addEventListener('click', e => {
     //get email and password
     const email = txtEmail.value;
     const pass = txtPassword.value;
-
     //sign in
     firebase.auth().signInWithEmailAndPassword(email, pass).catch(function (error) {
-        console.log(err)
+        window.alert("error")
     })
 })
+
+/////////////////////////////////////
+// If user clicks sign up button
+// authenticate them and then sign
+// them in
+////////////////////////////////////
+btnSignUp.addEventListener('click', event => {
+    const email = txtEmail.value;
+    const pass = txtPassword.value
+
+    firebase.auth().createUserWithEmailAndPassword(email, pass).then(() => {
+        window.alert("Thank you for signing up!")
+    }).catch(function (error) {
+        // Handle Errors here.
+
+    });
+})
+
+function logout() {
+    alert("clicked")
+    firebase.auth().signOut()
+}
