94733bdb0e1621120c3e363f0cb10e9785f09ae3
gjshirey <galenshirey@gmail.com>
2020-03-05 20:54:35 -0500

    commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 // Edited By: 
 // Creation Date: 3.1.2020
 // app.js - main javascript source file
-// Version: 0.3.3
+// Version: 0.4.5
 //      (major.minor.update)
 /////////////////////////////////////////
 
